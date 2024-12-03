93940ad685013e5588aafc093893d6e879cb30ce
Kyle Ryc <kyle.ryc@gmail.com>
2024-12-03 23:16:26 +0000

    Make a plan

---
+++ day1/day1.py
@@ -0,0 +1,7 @@
+# Steps:
+# - Import the data from input
+# - Create two lists: one of the left and one for the right.
+# - Sort each list from least to greatest.
+# - Compare the difference between the nth element of left and the nth element of right.
+# - Add this difference to a running total.
+
